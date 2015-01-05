a66e2120ddab9732175b572cfb26fc372fbc3445
Michail Pishchagin <mblsha@yandex-team.ru>
2015-01-05 12:39:03 +0300

    Test HttpRequest.params.

--- Common/HttpRequest.swift
+++ Common/HttpRequest.swift
@@ -13,4 +13,18 @@ public struct HttpRequest {
     public let headers: [String: String]
     public let body: String?
     public var capturedUrlGroups: [String]
+
+    public func params(name: String) -> [String] {
+      return reduce(urlParams, []) {
+        if $1.0 == name {
+          return $0 + [$1.1]
+        } else {
+          return $0
+        }
+      }
+    }
+
+    public func param(name: String) -> String? {
+      return params(name).first
+    }
 }
---
+++ SwifterTests/HttpRequestTests.swift
@@ -0,0 +1,23 @@
+//
+//  HttpRequestTests.swift
+//  Swifter
+//
+//  Created by Michail Pishchagin on 05.01.15.
+//  Copyright (c) 2015 Damian Kołakowski. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class HttpRequestTests: XCTestCase {
+  func httpRequest(params: [(String,String)]) -> HttpRequest {
+    return HttpRequest(url: "", urlParams: params, method: "GET", headers: [String:String](), body: nil, capturedUrlGroups: [])
+  }
+
+  func testUrlParams() {
+    XCTAssertEqual(httpRequest([("foo", "bar")]).param("foo")!, "bar")
+    XCTAssert(httpRequest([("foo", "bar")]).param("baz") == nil)
+    XCTAssertEqual(httpRequest([("foo", "bar"), ("foo", "baz")]).param("foo")!, "bar")
+    XCTAssertEqual(httpRequest([("foo", "bar"), ("foo", "baz")]).params("foo"), ["bar", "baz"])
+  }
+}
