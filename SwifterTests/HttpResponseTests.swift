e998a27b346ddb25b3165793afb7b376987546ae
Michail Pishchagin <mblsha@yandex-team.ru>
2015-01-05 17:03:11 +0300

    Test Json encoding.

---
+++ SwifterTests/HttpResponseTests.swift
@@ -0,0 +1,17 @@
+//
+//  HttpResponseTests.swift
+//  Swifter
+//
+//  Created by Michail Pishchagin on 05.01.15.
+//  Copyright (c) 2015 Damian Kołakowski. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class HttpResponseTests: XCTestCase {
+  func testHttpResponseBodyJson() {
+    XCTAssertEqual(HttpResponseBody.JSON(["foo": "bar"]).data()!, "{\"foo\":\"bar\"}")
+    XCTAssertEqual(HttpResponseBody.JSON(["foo": "b/a/r"]).data()!, "{\"foo\":\"b\\/a\\/r\"}")
+  }
+}
